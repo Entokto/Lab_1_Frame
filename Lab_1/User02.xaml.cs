b4daa8f50afb0b85900fe605f4fc711c662de42e
Арина <@DESKTOP-DIP1JMV>
2024-02-05 03:12:47 +0300

    Добавьте файлы проекта.

---
+++ Lab_1/User02.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab_1
+{
+    /// <summary>
+    /// Логика взаимодействия для User02.xaml
+    /// </summary>
+    public partial class User02 : Page
+    {
+        public User02()
+        {
+            InitializeComponent();
+        }
+    }
+}
